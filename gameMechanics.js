15b3ca049426103a48b0d121515d5d4035df0589
vandhear <40018995+vandhear@users.noreply.github.com>
2018-12-27 00:00:30 +0200

    Add files via upload

--- gameMechanics.js
+++ gameMechanics.js
@@ -1,5 +1,5 @@
-let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
@@ -7,16 +7,24 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
+let radiusOfTheBall = 10;
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   x += dx;
   y += dy;
   drawBall();
+
+  if (y + dy < radiusOfTheBall || y + dy > canvas.height - radiusOfTheBall) {
+      dy = -dy;
+  } else if (x + dx < radiusOfTheBall || x + dx > canvas.width - radiusOfTheBall) {
+      dx = -dx;
+  }
 }
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, radiusOfTheBall, 0, Math.PI * 2);
     ctx.fillStyle = 'yellow';
     ctx.fill();
     ctx.closePath();
